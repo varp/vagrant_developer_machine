bb07f9b72b8d97085a7690ada1da4884c4ac458c
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-06 19:11:21 +0600

    pip.sh script coded

--- devtools/languages/python/pip.sh
+++ devtools/languages/python/pip.sh
@@ -2,16 +2,28 @@ echo "Warning! It will install latest version of [pip] and [setuptools]\nNot dis
 
 download_ez_setup()
 {
+     curl -3 -sSL https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -o /tmp/ez_setup.py
 }
 
 install_ez_setup()
 {
+     python /tmp/ez_setup.py
 }
 
 download_get_pip()
 {
+    curl -3 -sSL https://raw.github.com/pypa/pip/master/contrib/get-pip.py -o /tmp/get-pip.py
 }
 
 install_pip()
 {
+    python /tmp/get-pip.py
+}
+
+run()
+{
+    download_ez_setup
+    install_ez_setup
+    download_get_pip
+    install_pip
 }
