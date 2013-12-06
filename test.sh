b7a9dba672237a65bad73d0c24802b00590a5e2e
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-06 14:50:14 +0600

    changed test for vagrant provision

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-test_file=test_vagran_provision
+test_file=test_vagrant_provision
 
-[ -f $HOME/test_file ] && echo 'test OK!'
+[ -f $HOME/$test_file ] && echo 'test OK!'
 
-[ ! -f $HOME/test_file ] && touch test_vagrant_provision
+[ ! -f $HOME/$test_file ] && touch  $test_file
