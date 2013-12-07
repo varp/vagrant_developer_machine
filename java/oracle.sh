12623a24d34be822ea64659635d8a5b0c7d02fef
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-07 17:23:14 +0600

    added oracle java module

---
+++ java/oracle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+add_webupd_ppa()
+{
+  add-apt-repository -y ppa:webupd8team/java
+  apt-get update
+}
+
+install_oracle_installer()
+{
+  # TODO accept Oracle Java license ## dpkg-config
+  apt-get install oracle-java7-installer 
+}
+
+add_webupd_ppa
+install_oracle_installer
