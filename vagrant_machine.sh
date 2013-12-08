0bbf6221ffb72eeb68fd18483d431b24f7697b7b
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-08 21:05:45 +0600

    fix

--- vagrant_machine.sh
+++ vagrant_machine.sh
@@ -4,8 +4,8 @@ SCRIPTS_ROOT='/vagrant/vagrant_provision'
 
 cd $SCRIPTS_ROOT
 
-# ./system/upgrade.sh
-# ./devtools/git.sh
+./system/upgrade.sh
+./devtools/git.sh
 ./devtools/vim.sh
 ./configs/configs.sh locales
 
