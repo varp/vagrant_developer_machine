f6fd5555ee7eae6f1df26ec307e933bca74b0877
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-09 18:38:30 +0600

    small fixes

--- system/upgrade.sh
+++ system/upgrade.sh
@@ -6,4 +6,10 @@ upgrade_system()
  apt-get update && apt-get dist-upgrade -y 
 }
 
+upgrade_vbox_guest_additions()
+{
+  apt-get install -y virtualbox-guest-additions virtualbox-guest-utils
+}
+
 upgrade_system
+upgrade_vbox_guest_additions
