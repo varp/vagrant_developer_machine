4476c8862d4f8d8076749c2ad7746088a7d74b1c
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-09 14:21:10 +0600

    fixed hostname

--- system/hostname.sh
+++ system/hostname.sh
@@ -5,7 +5,7 @@ update_hostname()
 	machine_num=$(head -c 1 /dev/random | od -t u1 | awk '{for (i = 2; i <= NF; i++) print $i}')
 	machine_name="developer-pc-$machine_num"
 
-	hostname -s $machine_name
+	hostname $machine_name
 }
 
 update_hostname
