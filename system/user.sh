61f0e1e455dba2cb4476c7ba27d76ed386a5f5a1
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-09 20:25:27 +0600

    removed error option in user.sh

--- system/user.sh
+++ system/user.sh
@@ -2,7 +2,7 @@
 
 add_developer_user()
 {
-	useradd -a -m -s /bin/bash -G adm,sudo,dip,plugdev,lpadmin,sambashare,admin,vboxsf developer
+	useradd -m -s /bin/bash -G adm,sudo,dip,plugdev,lpadmin,sambashare,admin,vboxsf developer
 	echo "INFO: Enter password for your developer user"
 	passwd developer
 }
