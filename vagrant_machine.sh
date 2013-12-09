a7600fd910baadb3ad20b74731d3c8f7918c29a9
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-09 18:06:22 +0600

    added sample Vagrantfile

--- vagrant_machine.sh
+++ vagrant_machine.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mount -t vboxsf -o uid=`id -u vagrant`,gid=`getent group vagrant | cut -d: -f3` /vagrant /vagrant
-
-
 SCRIPTS_ROOT='/vagrant/vagrant_provision'
 export DEBIAN_FRONTEND=readline
 
