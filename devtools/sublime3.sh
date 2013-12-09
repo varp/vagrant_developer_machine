bdd6a409769a5e0d8361cdcb82d0d2b69bc15f7f
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-09 12:45:17 +0600

    added modules for sublime text 2-3

--- devtools/sublime3.sh
+++ devtools/sublime3.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+install_sublime3()
+{
+	add-apt-repository -y ppa:webupd8team/sublime-text-3
+	apt-get update
+	apt-get install -y sublime-text-installer
+}
+
+install_sublime3
--- system/repo_yandex.sh
+++ system/repo_yandex.sh
@@ -3,7 +3,7 @@
 change_repo_mirror_to_yandex()
 {
     repo_conf=sources.list
-    repo_path=/etc/apt/
+    repo_path=/etc/apt
     tmp_repo=/tmp/new_repo.list
 
     sed -e 's/us\.archive\.ubuntu\.com/mirror.yandex.ru/g' $repo_path/$repo_conf > $tmp_repo
