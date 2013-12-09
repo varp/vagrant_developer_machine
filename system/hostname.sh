8a13f8153b2dcef24f46153970ef4e08410327f5
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-09 14:03:27 +0600

    added hostname module

---
+++ system/hostname.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+update_hostname()
+{
+	machine_num=$(head -c 1 /dev/random | od -t u1 | awk '{for (i = 2; i <= NF; i++) print $i}')
+	machine_name="developer-pc-$machine_num"
+
+	hostname -s machine_name
+}
+
+update_hostname
