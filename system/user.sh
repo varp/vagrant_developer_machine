f1703570ab7838d4e8a4d341165228cbf25aa676
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-10 11:12:53 +0600

    unneeded echo removed

--- system/user.sh
+++ system/user.sh
@@ -3,7 +3,6 @@
 add_developer_user()
 {
 	useradd -m -s /bin/bash -G adm,sudo,dip,plugdev,lpadmin,sambashare,admin,vboxsf developer
-	echo "INFO: Enter password for your developer user"
 }
 
 add_developer_user
