3f0004d448608f976f1a2967568ba647ff70c78e
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2013-12-16 13:14:02 +0600

    crlf fix

--- deps/zippers.sh
+++ deps/zippers.sh
@@ -2,7 +2,7 @@
 
 install_zippers()
 {
-  apt-get install -y -q gzip bzip2 p7zip zlib1g zlib1g-dev tar unrar
+  apt-get install -y -q gzip bzip2 p7zip-full zlib1g zlib1g-dev tar unrar
 }
 
 install_zippers
