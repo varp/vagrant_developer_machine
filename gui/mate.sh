ca96b9c6a98dd1e81ed7ee3508ae286ea1efef1a
Vardan Pogosyan <vardan.pogosyan@gmail.com>
2014-03-13 09:59:17 +0600

    add mate gui install

--- gui/mate.sh
+++ gui/mate.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-# TODO
+install_mate_desktop()
+{
+  add-apt-repository "deb http://packages.mate-desktop.org/repo/ubuntu $( lsb_release -cs) main"
+  apt-get update
+  apt-get install mate-archive-keyring
+  apt-get update
+  # this installs base packages
+  apt-get install mate-core
+  # this installs more packages
+  apt-get install mate-desktop-environment
+}
+
+install_mate_desktop
